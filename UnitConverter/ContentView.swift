8c223c4185941ccc15803aa4744ddfe5f5faf63e
hugofalkman <hugo@falkman.net>
2019-10-14 12:41:35 +0200

    Initial Commit

---
+++ UnitConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnitConverter
+//
+//  Created by H Hugo Falkman on 14/10/2019.
+//  Copyright © 2019 H Hugo Falkman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
